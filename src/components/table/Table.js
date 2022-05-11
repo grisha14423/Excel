71766fb0433f988b18deb078265b279df4e671bc
Grigory <grisha14423@gmail.com>
2022-05-11 19:19:23 +0300

    finish base framework functional

---
+++ src/components/table/Table.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import {ExcelComponent} from '../../core/ExcelComponent';
+import {createTable} from "./table.template";
+
+export class Table extends ExcelComponent {
+    static className = 'excel__table'
+
+    toHTML() {
+        return createTable(20)
+    }
+}
