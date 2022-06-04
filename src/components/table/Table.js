419223af2f12f207a5d04416158b468d056d8150
Grigory <grisha14423@gmail.com>
2022-06-04 21:41:30 +0300

    finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
 import {ExcelComponent} from '../../core/ExcelComponent';
 import {createTable} from "./table.template";
+import {resizeHandler} from "./table.resize";
+import {shouldResize} from "./table.functions";
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
 
+    constructor($root) {
+        super($root, {
+            listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+        });
+    }
+
     toHTML() {
         return createTable(20)
     }
+
+    onClick() {
+        console.log('onClick')
+    }
+
+    onMousedown(event) {
+        if (shouldResize(event)) {
+            resizeHandler(this.$root, event)
+        }
+    }
+
+    // onMouseup() {
+    //     console.log('mouseup')
+    // }
+    //
+    // onMousemove() {
+    //     // console.log('mousemove')
+    // }
 }
---
+++ src/components/table/table.functions.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+
+
+export function shouldResize(event) {
+    return event.target.dataset.resize
+}
