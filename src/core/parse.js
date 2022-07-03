c9aa64edcba0fd924ee1c8258683db9e6751fc6e
Grigory <grisha14423@gmail.com>
2022-07-03 11:46:51 +0300

    finish redux and table logic

---
+++ src/core/parse.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+export function parse(value = '') {
+  if (value.startsWith('=')) {
+    try {
+      return eval(value.slice(1))
+    } catch (e) {
+      return value
+    }
+  }
+  return value
+}
+
