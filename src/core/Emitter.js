93ddf189c762531ac4e101dffa62dc9c949f687a
Grigory <grisha14423@gmail.com>
2022-07-03 11:42:06 +0300

    finish redux and table logic

--- src/core/Emitter.js
+++ src/core/Emitter.js
@@ -1,34 +1,50 @@
 /* eslint-disable */
-
 export class Emitter {
-    constructor() {
-        this.listeners = {}
+  constructor() {
+    this.listeners = {}
+  }
+
+  // dispatch, fire, trigger
+  // Уведомляем слушателе если они есть
+  // table.emit('table:select', {a: 1})
+  emit(event, ...args) {
+    if (!Array.isArray(this.listeners[event])) {
+      return false
     }
+    this.listeners[event].forEach(listener => {
+      listener(...args)
+    })
+    return true
+  }
 
-    // dispatch Уведомление о слушателе
-    emit(event, ...args) {
-        if(!Array.isArray(this.listeners[event])) return false
-
-        this.listeners[event].forEach(listener => {
-            listener(...args)
-        })
-        return true
+  // on, listen
+  // Подписываемся на уведомление
+  // Добавляем нового слушателя
+  // formula.subscribe('table:select', () => {})
+  subscribe(event, fn) {
+    this.listeners[event] = this.listeners[event] || []
+    this.listeners[event].push(fn)
+    return () => {
+      this.listeners[event] =
+        this.listeners[event].filter(listener => listener !== fn)
     }
-
-    // on, listen Подписаться на уведомление, добавить нового слушателя
-    subscribe(event, fn) {
-        this.listeners[event] = this.listeners[event] || []
-        this.listeners[event].push(fn)
-
-        return () => {
-            this.listeners[event] = this.listeners[event].filter(listener => listener !== fn)
-        }
-    }
+  }
 }
 
+// Example
 // const emitter = new Emitter()
-// emitter.subscribe('myEvent', data => console.log('subscriber:', data))
-// const unsub = emitter.subscribe('myEvent', data => console.log('subscriber:', data))
-// emitter.emit('myEvent', 'pulsar1')
-// unsub()
-// emitter.emit('myEvent', 'pulsar3')
+//
+// const unsub = emitter.subscribe('vladilen', data => console.log(data))
+// emitter.emit('1231231', 42)
+//
+// setTimeout(() => {
+//   emitter.emit('vladilen', 'After 2 seconds')
+// }, 2000)
+//
+// setTimeout(() => {
+//   unsub()
+// }, 3000)
+//
+// setTimeout(() => {
+//   emitter.emit('vladilen', 'After 4 seconds')
+// }, 4000)
